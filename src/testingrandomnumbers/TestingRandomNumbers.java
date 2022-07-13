2f2de9e6580fc1ea5511b572907f622ee6cdd4ab
Kevin Bell <bellKevin@pm.me>
2022-07-13 02:51:15 +0000

    Update TestingRandomNumbers.java

--- src/testingrandomnumbers/TestingRandomNumbers.java
+++ src/testingrandomnumbers/TestingRandomNumbers.java
@@ -1,13 +1,10 @@
+/** ****************************************************************************
+ * TestingRandomNumbers.java
+ * Kevin Bell
+ * This program generates a random number within a range and exists only to
+ * practice unit testing
+ **************************************************************************** */
 package testingrandomnumbers;
-
-/******************************************************************************
-* TestingRandomNumbers.java 
-* Programmer: @author jcboyd
-* Version: 1.0
-* Course: SDEV 2210
-* This program generates a random number within a range and exists only to 
-* practice unit testing
-******************************************************************************/
 
 import java.util.*; //Scanner, Random
 
@@ -18,20 +15,15 @@ public class TestingRandomNumbers {
         int answer = rng.nextInt((max - min) + 1) + min;
         return answer;
     } //end getRN
-    
-    public static void main(String[] args) {
 
-        System.out.println("Proj 7: Testing Random Numbers by JC Boyd\n");
-        
-        Scanner stdIn = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Proj 7: Testing Random Numbers by Kevin Bell\n");
+        Scanner computerKeyboardInput = new Scanner(System.in);
         int min, max, answer;
-        
         System.out.print("Enter minimum random number: ");
-        min = Integer.parseInt(stdIn.nextLine());
+        min = Integer.parseInt(computerKeyboardInput.nextLine());
         System.out.print("Enter maximum random number: ");
-        max = Integer.parseInt(stdIn.nextLine());
-        
+        max = Integer.parseInt(computerKeyboardInput.nextLine());
         System.out.printf("The random number is: %d\n", getRN(min, max));
     } //end main
-    
 } //end class TestingRandomNumbers
